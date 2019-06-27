8ef9a1308a06df24894610b53c44a35ee9e3213d
miro <736499127wawa@gmail.com>
2019-06-27 16:34:04 +0100

    modified 27/6

--- TL/test.cpp
+++ TL/test.cpp
@@ -1,9 +1,12 @@
 #include "test.h"
+#include <vector>
 
 int maxSteps = 100;
 double maxSpeed = 12.8;
 double maxvalue = 3000.0;
-
+double y2 = 50;
+double x2 = 50;
+double robot_radius = 3.7;
 
 
 double f(double x,double x1,double y1,double k)
@@ -17,23 +20,112 @@ double g(double y,double x1,double y1,double k)
 } 
 
 //calculate the real distance from replica to the wall
-unsigned int replica_cal_distance(Agent1* r)
+double replica_cal_distance(Agent1* r)
 {
-  double x1 = r->pos->x;
-  double y1 = r->pos->y;
+  double x1 = r->pos.x;
+  double y1 = r->pos.y;
   double k = tan(r->angle);
+  Enki::point new_pos;
 
-
-  //if parallel
-
+ //if parallel
+  if((r->angle==0)||(r->angle==2.*M_PI)){
+    new_pos = Enki::point(x2,f(x2,x1,y1,k));
+    return (r->pos-new_pos).norm()-robot_radius;
+  } else if(r->angle==M_PI){
+    new_pos = Enki::point(0,f(0,x1,y1,k));
+    return (r->pos-new_pos).norm()-robot_radius;
+  } else if(r->angle==0.5*M_PI){
+    new_pos = Enki::point(g(y2,x1,y1,k),y2);
+    return (r->pos-new_pos).norm()-robot_radius;
+  } else if(r->angle==1.5*M_PI){
+    new_pos = Enki::point(g(0,x1,y1,k),0);
+    return (r->pos-new_pos).norm()-robot_radius;
+  } else{
   //if not parallel
+  //find the intersecting points
+    Enki::point points[4]={
+      Enki::point(g(y2,x1,y1,k),y2),
+      Enki::point(g(0,x1,y1,k),0),
+      Enki::point(0,f(0,x1,y1,k)),
+      Enki::point(x2,f(x2,x1,y1,k))
+    };
+    std::vector<int>check_inner;
+
+    if(g(y2,x1,y1,k)>=0 && g(y2,x1,y1,k)<=x2){inner_check.push_back(0);}
+    if(g(0,x1,y1,k)>=0 && g(0,x1,y1,k)<=x2){inner_check.push_back(1);}
+    if(f(0,x1,y1,k)>=0 && f(0,x1,y1,k)<=y2){inner_check.push_back(2);}
+    if(f(x2,x1,y1,k)>=0 && f(x2,x1,y1,k)<=y2){inner_check.push_back(3);}
+
+    if((r->angle>0)&&(r->angle<M_PI)){
+      if(points[inner_check[1]].y>points[inner_check[2]].y){
+        return (r->pos-points[inner_check[1]]).norm()-robot_radius;
+      }else{
+        return (r->pos-points[inner_check[2]]).norm()-robot_radius;
+      }
+    }else if((r->angle>M_PI)&&(r->angle<2.*M_PI)){
+      if(points[inner_check[1]].y>points[inner_check[2]].y){
+        return (r->pos-points[inner_check[2]]).norm()-robot_radius;
+      }else{
+        return (r->pos-points[inner_check[1]]).norm()-robot_radius;
+      }
+    } 
+
+  } 
    
-
 }
-//simlate the sensor readings of tof sensor on the agent 
-unsigned int agent_get_reading(Agent* a)
-{
 
+//simlate the sensor readings of tof sensor on the agent 
+double agent_get_reading(Agent* a)
+{
+  double x1 = a->pos.x;
+  double y1 = a->pos.y;
+  double k = tan(a->angle);
+  Enki::point new_pos;
+
+ //if parallel
+  if((a->angle==0)||(a->angle==2.*M_PI)){
+    new_pos = Enki::point(x2,f(x2,x1,y1,k));
+    return (a->pos-new_pos).norm()-robot_radius;
+  } else if(a->angle==M_PI){
+    new_pos = Enki::point(0,f(0,x1,y1,k));
+    return (a->pos-new_pos).norm()-robot_radius;
+  } else if(a->angle==0.5*M_PI){
+    new_pos = Enki::point(g(y2,x1,y1,k),y2);
+    return (a->pos-new_pos).norm()-robot_radius;
+  } else if(a->angle==1.5*M_PI){
+    new_pos = Enki::point(g(0,x1,y1,k),0);
+    return (a->pos-new_pos).norm()-robot_radius;
+  } else{
+  //if not parallel
+  //find the intersecting points
+    Enki::point points[4]={
+      Enki::point(g(y2,x1,y1,k),y2),
+      Enki::point(g(0,x1,y1,k),0),
+      Enki::point(0,f(0,x1,y1,k)),
+      Enki::point(x2,f(x2,x1,y1,k))
+    };
+    std::vector<int>check_inner;
+
+    if(g(y2,x1,y1,k)>=0 && g(y2,x1,y1,k)<=x2){inner_check.push_back(0);}
+    if(g(0,x1,y1,k)>=0 && g(0,x1,y1,k)<=x2){inner_check.push_back(1);}
+    if(f(0,x1,y1,k)>=0 && f(0,x1,y1,k)<=y2){inner_check.push_back(2);}
+    if(f(x2,x1,y1,k)>=0 && f(x2,x1,y1,k)<=y2){inner_check.push_back(3);}
+
+    if((a->angle>0)&&(a->angle<M_PI)){
+      if(points[inner_check[1]].y>points[inner_check[2]].y){
+        return (a->pos-points[inner_check[1]]).norm()-robot_radius;
+      }else{
+        return (a->pos-points[inner_check[2]]).norm()-robot_radius;
+      }
+    }else if((a->angle>M_PI)&&(a->angle<2.*M_PI)){
+      if(points[inner_check[1]].y>points[inner_check[2]].y){
+        return (a->pos-points[inner_check[2]]).norm()-robot_radius;
+      }else{
+        return (a->pos-points[inner_check[1]]).norm()-robot_radius;
+      }
+    } 
+
+  } 
 }
 
 void AgentBehaviour(const int &C_num)
@@ -45,7 +137,7 @@ void AgentBehaviour(const int &C_num)
   for (int Step = 0; Step < maxSteps; Step++)
   {
     world.run();
-    CF_input[8] = 1.0;
+    CF_input = agent_get_reading(Agent* a);
     CF_elmanNetwork(CF_input, C_num);
     a->leftSpeed = 2 * maxSpeed * CF_output[1] - maxSpeed;
     a->rightSpeed = 2 * maxSpeed * CF_output[2] - maxSpeed;
@@ -61,7 +153,7 @@ void ReplicaBehaviour(const int &C_num)
   for (int Step = 0; Step < maxSteps; Step++)
   {
     world.run();
-    CF_input[8] = 1.0;
+    CF_input = k*replica_cal_distance(Agent1* r)+b;
     CF_elmanNetwork(CF_input, C_num);
     r->leftSpeed = 2 * maxSpeed * CF_output[1] - maxSpeed;
     r->rightSpeed = 2 * maxSpeed * CF_output[2] - maxSpeed;
--- TL/test.h
+++ TL/test.h
@@ -9,11 +9,11 @@
 void AgentBehaviour(const int &C_num);
 void ReplicaBehaviour(const int &C_num);
 //calculate the real distance from replica to the wall
-unsigned int replica_cal_distance(Agent1* r);
+double replica_cal_distance(Agent1* r);
 //simlate the sensor readings of tof sensor on the agent 
-unsigned int agent_get_reading(Agent* a);
+double agent_get_reading(Agent* a);
 
-int f(int x);
-int g(int y); 
+double f(double x,double x1,double y1,double k);
+double g(double y,double x1,double y1,double k); 
 
 #endif
